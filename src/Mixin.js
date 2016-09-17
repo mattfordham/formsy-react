9f14f8082ff5decd71065c41c136dd982ef0af09
Matt Fordham <matt@wintr.us>
2016-09-17 13:09:57 -0700

    Build

--- src/Mixin.js
+++ src/Mixin.js
@@ -115,12 +115,14 @@ module.exports = {
   },
 
   // We validate after the value has been set
-  setValue: function (value) {
+  setValue: function (value, validate) {
     this.setState({
       _value: value,
       _isPristine: false
     }, function () {
-      this.context.formsy.validate(this);
+      if(validate !== false) {
+        this.context.formsy.validate(this);
+      }
       //this.props._validate(this);
     }.bind(this));
   },
